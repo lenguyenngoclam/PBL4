6b6ac053cf61c461b677a57473c40cbd8364d485
NGOC LAM <ngoclam@NGOCs-MacBook-Pro.local>
2022-11-04 21:52:46 +0700

    Complete first part

---
+++ src/main/java/screen/ScreenEvent.java
@@ -0,0 +1,15 @@
+package screen;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ScreenEvent extends Remote{
+    public boolean checkPassword(String inputPassword) throws RemoteException;
+	public byte[] sendScreen() throws RemoteException;
+	public double getWidth() throws RemoteException;
+	public double getHeight() throws RemoteException;
+	public void mouseMovedEvent(int xScale, int yScale) throws RemoteException;
+	public void mousePressedEvent(int buttonPressed) throws RemoteException;
+	public void mouseReleasedEvent(int buttonReleased) throws RemoteException;
+	public void keyPressed(int keyPressed) throws RemoteException;
+	public void keyReleased(int keyReleased) throws RemoteException;
+}
